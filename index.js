f2be69ba099a7fbec74b979db54a5bc51db51027
Cesar Campos <camposss.415@gmail.com>
2023-01-15 16:39:48 -0800

    activated the email services

--- index.js
+++ index.js
@@ -48,25 +48,25 @@ function contact(event) {
   const loading = document.querySelector('.modal__overlay--loading')
   const success = document.querySelector('.modal__overlay--success')
   loading.classList += " modal__overlay--visible"
-  // emailjs.sendForm(
-  //   "service_litwiq6",
-  //   "template_nltfluk",
-  //   event.target,
-  //   "XLo66Pj42bzrGVQqR"
-  // ).then(() => {
-  //   // throw new Error("error")
-  //   loading.classList.remove('modal__overlay--visible')
-  //   success.classList += " modal__overlay--visible"
-  // }).catch(() => {
-  //    loading.classList.remove('modal__overlay--visible')
-  //    alert (
-  //     'Email service is down :(  Please contact me directly at camposss.415@gmail.com'
-  //    )
-  // })
-  // comment out code below when  production
-  // product page is done!!  VERY IMPORTANT
-  setTimeout(() => {
+  emailjs.sendForm(
+    "service_litwiq6",
+    "template_nltfluk",
+    event.target,
+    "XLo66Pj42bzrGVQqR"
+  ).then(() => {
+    // throw new Error("error")
     loading.classList.remove('modal__overlay--visible')
     success.classList += " modal__overlay--visible"
-  }, 1000)
+  }).catch(() => {
+     loading.classList.remove('modal__overlay--visible')
+     alert (
+      'Email service is down :(  Please contact me directly at camposss.415@gmail.com'
+     )
+  })
+  // comment out code below when  production
+  // product page is done!!  VERY IMPORTANT
+  // setTimeout(() => {
+  //   loading.classList.remove('modal__overlay--visible')
+  //   success.classList += " modal__overlay--visible"
+  // }, 1000)
 }
